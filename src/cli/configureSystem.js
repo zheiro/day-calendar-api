33f2c023897d4debab9c0564c1902d8c37c28f9d
zheiro <kabesino@gmail.com>
2018-06-11 04:08:01 +0800

    added git ignore and remove unnessary env

--- src/cli/configureSystem.js
+++ src/cli/configureSystem.js
@@ -7,38 +7,14 @@ import { pathExists, outputFile, readFile } from 'fs-extra';
 
 
 const items = {
-	// APP_BASE_URI: 'Web app public URL',
-	// UPLOAD_FILE_DIR: 'Where to store uploaded files',
 	DB_HOST: 'localhost',
 	DB_NAME: 'day_calendar',
 	DB_USERNAME: 'root',
 	DB_PASSWORD: 'root',
-	// APIC_BASE_URL: 'APIC endpoint base URL',
-	// APIC_CLIENT_ID: 'APIC Client ID',
-	// APIC_CLIENT_SECRET: 'APIC Client Secret',
-	// APIC_CALLBACK_URL: 'Callback URL for APIC',
-	// SMTP_HOST: 'Outgoing mail server hostname/IP address',
-	// SMTP_PORT: 'Outgoing mail server port',
-	// SMTP_USERNAME: 'Outgoing mail server username',
-	// SMTP_PASSWORD: 'Outgoing mail server password',
-	// KV_AZURE_SUBSCRIPTION_ID: 'Azure subscription ID',
-	// KV_CLIENT_ID: 'KeyVault client ID',
-	// KV_OBJECT_ID: 'KeyVault object ID',
-	// KV_APPLICATION_SECRET: 'KeyVault application secret',
-	// KV_DOMAIN: 'KeyVault domain',
-	// KV_OBJECT_ID_KEYVAULT_OPERATIONS: 'KeyVault operations object ID',
-	// KV_SP_KEYVAULT_OPERATIONS: 'KeyVault operations SP',
-	// KV_VAULT_NAME: 'KeyVault vault name',
-	// KV_KEY_NAME: 'KeyVault encryption key name',
-	// KV_KEY_ID: 'KeyVault encryption key ID',
-	// KV_KEY_TYPE: 'KeyVault encryption key type (usually RSA-OAEP)'
 };
 
 const secretFields = [
 	'DB_PASSWORD',
-	// 'APIC_CLIENT_SECRET',
-	// 'SMTP_PASSWORD',
-	// 'KV_APPLICATION_SECRET'
 ];
 
 export default async function configureSystem(prompt) {
